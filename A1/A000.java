5fd2c163e50cad4c70fd0246f7e8259ffe00a263
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-05-15 16:22:12 -0300

    Add files via upload

---
+++ A1/A000.java
@@ -0,0 +1,9 @@
+// Exercício 000: O clássico Hello World! :)
+
+package A1;
+
+public class A000 {
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+    }
+}
